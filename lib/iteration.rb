c04c16542a93c9f01ecbb58d0bca3b1f203f11c2
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-22 02:37:16 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,7 +11,7 @@ def join_ingredients(src)
   while row_index < src.count do
     # element_index = 0
     # while element_index < src[row_index].count do
-      toppings << "I love #{src[0]} and #{src[1]} on my pizza"
+      toppings << "I love #{src.first} and #{src.last} on my pizza"
     # end
     row_index += 1
   end
